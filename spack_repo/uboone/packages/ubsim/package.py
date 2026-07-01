f1df7441d6662acbb8c8952184bb056af8eae55b
Herbert Greenlee <greenlee@fnal.gov>
2026-07-01 13:55:12 -0500

    Update ubsim package.py.

--- spack_repo/uboone/packages/ubsim/package.py
+++ spack_repo/uboone/packages/ubsim/package.py
@@ -1,11 +1,6 @@
-# Copyright Spack Project Developers. See COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
+import sys, os
 from spack_repo.builtin.build_systems.cmake import CMakePackage
-
 from spack.package import *
-
 from spack_repo.fnal_art.packages.fnal_github_package.package import *
 
 class Ubsim(CMakePackage, FnalGithubPackage):
@@ -72,3 +67,10 @@ class Ubsim(CMakePackage, FnalGithubPackage):
 
     def url_for_version(self, version):
         return f"https://github.com/uboone/ubsim/archive/refs/tags/v{str(version).replace('.', '_')}.tar.gz"
+
+    def setup_run_environment(self, env):
+        print("Setting up ubsim run environment.", file=sys.stderr)
+
+        env.prepend_path("FHICL_FILE_PATH", os.path.join(self.prefix, "job"))
+
+        env.prune_duplicate_paths("FHICL_FILE_PATH")
